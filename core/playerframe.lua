071266d02d01b7f6529fe4285945a95867e2e85a
Jevan <jevanmeader@gmail.com>
2020-01-06 22:02:26 -0600

    updates to playerframe

--- core/playerframe.lua
+++ core/playerframe.lua
@@ -112,7 +112,7 @@ function uui_playerframes_LargeHealth(color)
 		PlayerFrameManaBar.FullPowerFrame.SpikeFrame.BigSpikeGlow:ClearAllPoints()
 		PlayerFrameManaBar.FullPowerFrame.SpikeFrame.BigSpikeGlow:SetPoint("CENTER",PlayerFrameManaBar.FullPowerFrame,"RIGHT",5,-4)
 		PlayerFrameManaBar.FullPowerFrame.SpikeFrame.BigSpikeGlow:SetSize(30, 50)
-	elseif uuidb.playerframe.largehealth and UnitInVehicle("Player") then
+	elseif uuidb.playerframe.largehealth and UnitInVehicle("Player") and PlayerFrameTexture:GetTexture() ~= "Interface\\Addons\\Uber UI\\textures\\target\\targetingframebig" then
 		PlayerFrameHealthBar:SetHeight(12)
 		PlayerFrameHealthBar.LeftText:ClearAllPoints()
 		PlayerFrameHealthBar.LeftText:SetPoint("LEFT", PlayerFrameHealthBar, "LEFT", 10, 0)
