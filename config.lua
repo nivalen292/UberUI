8f561dee84be106ab58b137e33e044f17bfecb84
Uberliciousx <jevanmeader@gmail.com>
2020-08-18 21:12:32 -0500

    update debuff Positioning lower

--- config.lua
+++ config.lua
@@ -156,7 +156,7 @@ local defaults = {
       },
     },
     debuff = {
-      pos               = {a1 = "TOPRIGHT", af = "rBFS_BuffDragFrame", a2 = "BOTTOMRIGHT", x = 0, y = -85 },
+      pos               = {a1 = "TOPRIGHT", af = "Minimap", a2 = "TOPLEFT", x = -35, y = -120 },
       gap               = 30,
       locked            = true,
       rowspacing        = 10,
