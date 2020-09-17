66b989b2fc4726ff1559f5a8dbb73bece1859baf
Uberliciousx <jevanmeader@gmail.com>
2020-09-17 12:40:27 -0500

    small format fix for nameplate numbering

--- core/arenaframes.lua
+++ core/arenaframes.lua
@@ -42,7 +42,8 @@ function arenaframes:NameplateNumbers()
 			if IsActiveBattlefieldArena() and F.unit:find("nameplate") then 
 				for i=1,5 do 
 					if U(F.unit,"arena"..i) then 
-						F.name:SetText(i)F.name:SetTextColor(1,1,0)
+						F.name:SetText(i)
+						F.name:SetTextColor(1,1,0)
 						break
 					end
 				end
